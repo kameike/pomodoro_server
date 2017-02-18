8eb833b61f4ddf9504646984051778c6501af31a
KamedaKei <kei1225kk@gmail.com>
2017-02-18 21:26:10 +0900

    wip :

--- src/post_data.hrl
+++ src/post_data.hrl
@@ -1,2 +1,15 @@
--record(timer, {start_date_time, duration, post_data, user_id}).
--record(post_data, {url, content}).
+-record(timer, {
+          start_date_time,
+          duration
+         }).
+
+-record(session, {
+          user_id,
+          session_id,
+          timers
+         })
+
+-record(post_data, {
+          url,
+          content
+         }).
--- util/post.sh
+++ util/post.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-# curl -w '\n' 'http://localhost:8080/createItem' --data 'name=sample&mode=hudson.model.FreeStyleProject&Submit=OK' -XPOST
-# curl -H 'Content-Type:application/json' -H 'User-Agent:iPhone' -H 'Accept-Encoding:gzip,deflate' -d "{"key":"val","key2":",val2"}" http://~~~~~~~~~~
 curl -v -X POST -H "Content-Type: application/json" -d '
 {
   "start_work_hock": {
